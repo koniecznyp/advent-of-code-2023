61209899acbc1342b40ba93f5bffe21fe333935c
Paweł Konieczny <pawel@MacBook-Air-Pawe.local>
2023-12-06 21:31:48 +0100

    day 6

---
+++ 06/Program.cs
@@ -0,0 +1,18 @@
+﻿var raceData = File.ReadAllLines("data.txt");
+var race = new Race(
+    long.Parse(raceData[0].Substring(raceData[0].IndexOf(":") + 1).Replace(" ", string.Empty)),
+    long.Parse(raceData[1].Substring(raceData[1].IndexOf(":") + 1).Replace(" ", string.Empty)));
+
+var beats = 0;
+for (long i=0; i<=race.Time; i++)
+{
+    var speed = i;
+    var route = race.Time - i;
+    var distance = speed * route;
+    if (distance > race.Distance)
+        beats++;
+}
+
+Console.WriteLine($"{beats}");
+
+public record Race(long Time, long Distance);
