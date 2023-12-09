4fe5abe35303e71821f0b742fafb19d09757f4ab
Paweł Konieczny <pawel@MacBook-Air-Pawe.local>
2023-12-09 23:05:13 +0100

    day 8 part 1

---
+++ 08/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] data = File.ReadAllLines("data.txt");
+
+char[] directions = data[0].ToCharArray();
+
+HashSet<Node> nodes = [];
+foreach(var row in data.Skip(2))
+{
+    var parent = row.Substring(0, 3);
+    var childs = row.Substring(row.IndexOf("=") +  1).Trim(' ', '(', ')').Split(",");
+
+    nodes.Add(new(parent, childs[0].Trim(), childs[1].Trim()));
+}
+
+var itemFound = false;
+int steps = 0;
+var currentNode = nodes.First();
+
+while(!itemFound)
+{
+    if (currentNode.Value.Equals("ZZZ"))
+    {
+        itemFound = true;
+        break;
+    }
+
+    currentNode = directions[steps % directions.Length] == 'L'
+        ? nodes.First(x => x.Value == currentNode.Left)
+        : nodes.First(x => x.Value == currentNode.Right);
+
+    steps++;
+}
+
+Console.WriteLine($"steps: {steps}");
+
+public record Node(string Value, string Left, string Right);
