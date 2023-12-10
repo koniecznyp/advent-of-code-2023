59d77d0129a29dd167f5126b8bb63301a9cac33e
Paweł Konieczny <pawel@MacBook-Air-Pawe.local>
2023-12-10 21:58:25 +0100

    day 9

---
+++ 09/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] data = File.ReadAllLines("data.txt");
+
+var sum = 0;
+foreach(var row in data)
+{
+    var history = row.Split(" ").Select(x=>int.Parse(x)).ToArray();
+    sum += GetNextNumber(history);
+}
+
+Console.WriteLine($"sum: {sum}");
+
+int GetNextNumber(int[] data)
+{
+    if (data.All(x => x == 0))
+        return 0;
+
+    int[] diffs = new int[data.Length - 1];
+    for(int i=0; i < data.Length - 1; i++)
+    {
+        var diff = data[i+1] - data[i];
+        diffs[i] = diff;
+    }
+
+    return data[0] - GetNextNumber(diffs);
+}
